6448c39fc1e25e68517d3b2c609c842026cb0f4d
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-10-15 14:48:24 +0000

    add old code for reference

--- random-photo/src/App.js
+++ random-photo/src/App.js
@@ -1,12 +1,42 @@
 import "./App.css";
 
+/* old code:
+
+// Show and hide loader
+function showLoader() {
+  loader.hidden = false;
+  innerContainer.hidden = true;
+}
+function hideLoader() {
+  loader.hidden = true;
+  innerContainer.hidden = false;
+}
+
+//Get photo from APOD API and show on page
 async function getPhoto() {
   const res = await fetch(
     "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1"
   );
   const dataArray = await res.json();
   const data = dataArray[0];
+
+  //console.log(data)
+  showLoader();
+  photoTitle.innerHTML = data.title;
+  photo.src = data.url;
+  photo.alt = data.title;
+  caption.innerHTML = data.explanation;
+  date.innerHTML = data.date;
+  if (data.copyright == undefined) {
+    copy.innerHTML = "";
+  } else {
+    copy.innerHTML = "&copy " + data.copyright;
+  }
+  hideLoader();
 }
+
+getPhoto();
+picBtn.addEventListener("click", getPhoto); */
 
 function App() {
   return (
@@ -20,9 +50,7 @@ function App() {
         <p className="caption">{data.explanation}</p>
         <p className="copy">{data.copyright}</p>
         <p className="copy">{data.date}</p>
-        <button className="btn" onClick={getPhoto}>
-          Get Another Photo
-        </button>
+        <button className="btn">Get Another Photo</button>
       </div>
     </div>
   );
