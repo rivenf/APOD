f5ff169fcde0e4d772059175ec99a0d3019e0a1f
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-10-15 14:21:36 +0000

    create component structure

--- random-photo/src/App.js
+++ random-photo/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+async function getPhoto() {
+  const res = await fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1"
+  );
+  const dataArray = await res.json();
+  const data = dataArray[0];
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src={data.url} alt={data.title} class="bgImg" />
+
+      <div className="container">
+        <h1>Get A Random Astronomy Photo</h1>
+        {/* <div class="loader" id="loader"></div> */}
+        <h2>{data.title}</h2>
+        <p className="caption">{data.explanation}</p>
+        <p className="copy">{data.copyright}</p>
+        <p className="copy">{data.date}</p>
+        <button className="btn" onClick={getPhoto}>
+          Get Another Photo
+        </button>
+      </div>
     </div>
   );
 }
