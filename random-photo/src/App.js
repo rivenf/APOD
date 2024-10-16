d39102bbaf9987d351e8a473eff79b4f845e81b2
Rivenf <39199060+rivenf@users.noreply.github.com>
2024-10-16 17:48:37 +0000

    Fix loading animation

--- random-photo/src/App.js
+++ random-photo/src/App.js
@@ -3,15 +3,18 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [photo, setPhoto] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getPhoto() {
+      setIsLoading(true);
       const res = await fetch(
         "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=1"
       );
       const dataArray = await res.json();
       const data = dataArray[0];
       setPhoto(data);
+      setIsLoading(false);
     }
     getPhoto();
   }, []);
@@ -19,15 +22,18 @@ function App() {
   return (
     <div className="app">
       <img src={photo.url} alt={photo.title} class="bgImg" />
-      {/* <div class="loader" id="loader"></div> */}
-      <div className="container">
-        <h1>Get A Random Astronomy Photo</h1>
-        <h2>{photo.title}</h2>
-        <p className="caption">{photo.explanation}</p>
-        <p className="copy">{photo.copyright}</p>
-        <p className="copy">{photo.date}</p>
-        <button className="btn">Get Another Photo</button>
-      </div>
+      {isLoading ? (
+        <div class="loader" id="loader"></div>
+      ) : (
+        <div className="container">
+          <h1>Get A Random Astronomy Photo</h1>
+          <h2>{photo.title}</h2>
+          <p className="caption">{photo.explanation}</p>
+          <p className="copy">{photo.copyright}</p>
+          <p className="copy">{photo.date}</p>
+          <button className="btn">Get Another Photo</button>
+        </div>
+      )}
     </div>
   );
 }
